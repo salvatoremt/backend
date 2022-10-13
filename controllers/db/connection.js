63dd62475428de41afeed04bfb3145c57357967c
salvatore monggiotti tassinari <isc18350507@gmail.com>
2022-10-13 10:30:57 -0500

    segundo comentario

---
+++ controllers/db/connection.js
@@ -0,0 +1,9 @@
+const mariadb =  requiere ("mariadb")
+
+const config ={
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_NAME,
+    database: process.env.DB_PORT,
+    connectionlimit: process.env.DB_CONN_LIMIT
+}
