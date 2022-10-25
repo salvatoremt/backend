48c697f012a2ee65e18715fc86e1e172196a8f27
salvatore monggiotti tassinari <isc18350507@gmail.com>
2022-10-25 10:45:42 -0500

    codigo actualizado

---
+++ controllers/usuario.js
@@ -0,0 +1,16 @@
+const { request, response } = require("express");
+
+const getUsers = (req = request, res = response) => {
+    console.log("funcion getUser")
+    res.json({msg: "Funcion getUsers"})
+}
+
+module.exports = {getUsers}
+
+
+
+
+
+
+
+
